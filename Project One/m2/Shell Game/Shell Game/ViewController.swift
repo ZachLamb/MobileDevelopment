a946bf82ac99e625c236979a8c94cd2be4087fe5
Zach Lamb <zala3440@colorado.edu>
2016-09-28 21:50:37 -0600

    added mock-up,icons,launch screen, some sudo-code,some stuff for the view,and link to high-fidelity mock-up

---
+++ Project One/m2/Shell Game/Shell Game/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Shell Game
+//
+//  Created by Zach Lamb on 9/28/16.
+//  Copyright © 2016 Zach Lamb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    //object
+    //cups
+    //buttons
+    //alerts
+    //labels
+    //score
+//    alert with table view
+//    function addNewUser(textField){
+//        create new user here
+//    }
+//    function selectExistingUser(table){
+//        return selected user
+//    }
+//    
+//    signOut(){
+//    sign out user and return to alert asking user to sign-in/up
+//    }
+//    
+//    putObjectUnderCup(){
+//    call random function to put object undercup
+//    }
+//    selectedCup(){
+//    return if it is the right cup, and display result
+//    also, update score if user selected correct cup
+//    }
+}
+
