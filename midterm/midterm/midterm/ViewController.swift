bba9157bad74fdaca0ce4dcefc93e786a65721a8
Zach Lamb <zala3440@colorado.edu>
2016-10-14 14:38:02 -0600

    finished 1, 2.a, 2.c,3,4

--- midterm/midterm/midterm/ViewController.swift
+++ midterm/midterm/midterm/ViewController.swift
@@ -13,9 +13,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var workoutTime: UITextField!
     @IBOutlet weak var miles: UILabel!
     @IBOutlet weak var calories: UILabel!
+    @IBOutlet weak var workoutImage: UIImageView!
+    @IBOutlet weak var workoutType: UISegmentedControl!
+    @IBOutlet weak var weekSwitch: UISwitch!
+    
     let pacePerMins = 10.00 //miles per min
     let pacePerHr = 6.00 //miles per hour
     let caloriesPerHour = 600.00
+    let bikePace = 4.00
+    let bikeCals = 510.00
+    let swimPace = 30.00
+    let swimCals = 420.00
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +31,23 @@ class ViewController: UIViewController {
     }
 
     @IBAction func calculateWorkout(sender: AnyObject) {
+        if(Double(workoutTime.text!)! < 30.00){
+            //create a UIAlertController object
+            let alert=UIAlertController(title: "Warning", message: "This workout is less than 30 minutes", preferredStyle:
+                UIAlertControllerStyle.Alert)
+            //create a UIAlertAction object for the button
+            let cancelAction = UIAlertAction(title: "Cancel",
+                                             style:UIAlertActionStyle.Cancel, handler: nil)
+            //add the alert action to the alert object
+            alert.addAction(cancelAction)
+        let okAction = UIAlertAction(title: "OK", style:
+            UIAlertActionStyle.Default, handler: { action in
+                self.workoutTime.text="30"
+        })
+        alert.addAction(okAction)
+        //present alert
+        presentViewController(alert, animated: true, completion: nil)
+        }
         miles.text=String(format: "%.01f",pacePerMins/Double(workoutTime.text!)!)
         calories.text=String(format: "%.01f",caloriesPerHour/Double(workoutTime.text!)!)
     }
@@ -32,6 +57,47 @@ class ViewController: UIViewController {
     }
     @IBAction func textFieldShouldReturn(sender: AnyObject) {
     }
-
+    @IBAction func workoutType(sender: UISegmentedControl) {
+        if(sender.selectedSegmentIndex==0){
+            //run
+            workoutImage.image=UIImage(named: "run.png")
+            miles.text=String(format: "%.01f",pacePerMins/Double(workoutTime.text!)!)
+            calories.text=String(format: "%.01f",caloriesPerHour/Double(workoutTime.text!)!)
+        }
+        else if(sender.selectedSegmentIndex==1){
+            //swim
+            workoutImage.image=UIImage(named: "swim.png")
+            miles.text=String(format: "%.01f",swimPace/Double(workoutTime.text!)!)
+            calories.text=String(format: "%.01f",swimCals/Double(workoutTime.text!)!)
+        }
+        else if(sender.selectedSegmentIndex==2){
+            //bike
+            workoutImage.image=UIImage(named: "bike.png")
+            miles.text=String(format: "%.01f",bikePace/Double(workoutTime.text!)!)
+            calories.text=String(format: "%.01f",bikeCals/Double(workoutTime.text!)!)
+        }
+    }
+    @IBAction func switchDaysOrWeeks(sender: UISwitch) {
+        
+        if(weekSwitch.on){
+            
+            if(miles.text != "...."){
+                let numMiles = Double(miles.text!)!
+                miles.text=String(format: "%.01f",numMiles*5.0)
+                let numCalories = Double(calories.text!)!
+                calories.text=String(format: "%.01f",numCalories*5.0)
+            }
+        }
+        else if (!weekSwitch.on){
+            if(miles.text != "...."){
+                let numMiles = Double(miles.text!)!
+                miles.text=String(format: "%.01f",numMiles/5.0)
+                let numCalories = Double(calories.text!)!
+                calories.text=String(format: "%.01f",numCalories/5.0)
+            }
+        }
+        
+    }
+    
 }
 
