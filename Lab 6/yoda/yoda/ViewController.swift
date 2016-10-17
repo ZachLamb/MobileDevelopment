988e25b4e1ed690d580f3b158f9339be8f1d52c0
Zach Lamb <zala3440@colorado.edu>
2016-10-17 17:12:52 -0600

    added in pinch to zoom

--- Lab 6/yoda/yoda/ViewController.swift
+++ Lab 6/yoda/yoda/ViewController.swift
@@ -8,12 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
     @IBAction func handlePan(sender: UIPanGestureRecognizer) {
         let translation = sender.translationInView(view)
         sender.view!.center = CGPoint(x: sender.view!.center.x + translation.x, y: sender.view!.center.y + translation.y)
         sender.setTranslation(CGPointZero, inView: view)
+        
+        if sender.state == UIGestureRecognizerState.Ended {
+            let velocity = sender.velocityInView(self.view)
+            let magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y))
+            let slideMultiplier = magnitude / 200
+            let slideFactor = 0.1 * slideMultiplier
+            
+            var finalPoint = CGPoint(x:sender.view!.center.x + (velocity.x * slideFactor), y:sender.view!.center.y + (velocity.y * slideFactor))
+            finalPoint.x = min(max(finalPoint.x,0),self.view.bounds.size.width)
+            finalPoint.y = min(max(finalPoint.y, 0),self.view.bounds.size.width)
+            
+            UIView.animateWithDuration(Double(slideFactor * 2), delay: 0, options: UIViewAnimationOptions.CurveEaseOut, animations: {sender.view!.center = finalPoint} , completion: nil)
+        
+        }
+    }
+    
+    @IBAction func handlePinch(sender: UIPinchGestureRecognizer) {
+        sender.view!.transform = CGAffineTransformScale(sender.view!.transform,sender.scale,sender.scale)
+            sender.scale=1 //reset
+    }
+    @IBAction func handleRotate(sender: UIRotationGestureRecognizer) {
+        sender.view!.transform = CGAffineTransformRotate(sender.view!.transform, sender.rotation)
+        sender.rotation=0 //reset
+        
     }
     
     override func viewDidLoad() {
