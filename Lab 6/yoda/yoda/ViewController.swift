ed973ee28e20b6944ba167d61315ed2a95342954
Zach Lamb <zala3440@colorado.edu>
2016-10-17 21:03:13 -0600

    finished lab 6

--- Lab 6/yoda/yoda/ViewController.swift
+++ Lab 6/yoda/yoda/ViewController.swift
@@ -7,6 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
+
+var audioPlayer : AVAudioPlayer?
+
+
 
 class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
@@ -30,6 +35,14 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         }
     }
     
+    @IBAction func handleLongPress(sender: UILongPressGestureRecognizer) {
+        let audioFilePath = NSBundle.mainBundle().pathForResource("Tributaries_of_the_Bayou_Sting", ofType: "mp3")
+        let fileURL = NSURL(fileURLWithPath: audioFilePath!)
+        audioPlayer = try? AVAudioPlayer(contentsOfURL: fileURL)
+        if audioPlayer != nil{
+            audioPlayer!.play()
+        }
+    }
     @IBAction func handlePinch(sender: UIPinchGestureRecognizer) {
         sender.view!.transform = CGAffineTransformScale(sender.view!.transform,sender.scale,sender.scale)
             sender.scale=1 //reset
@@ -39,6 +52,11 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         sender.rotation=0 //reset
         
     }
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
