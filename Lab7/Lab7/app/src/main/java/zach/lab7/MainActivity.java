9e0e86adb5873c7437559c79c3567543f52249a0
Zach Lamb <zala3440@colorado.edu>
2016-11-09 23:32:33 -0700

    finished lab7

--- Lab7/Lab7/app/src/main/java/zach/lab7/MainActivity.java
+++ Lab7/Lab7/app/src/main/java/zach/lab7/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -17,23 +19,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void lorelaiGilmore(View view){
+
+        double percent = 0.0;
+
         EditText name = (EditText) findViewById(R.id.nameEditText);
         String nameValue = name.getText().toString();
-        TextView feeling = (TextView) findViewById(R.id.feelingTextView);
-        Spinner moodSpinner = (Spinner) findViewById(R.id.spinner);
-        String moodValue = String.valueOf(moodSpinner.getSelectedItem());
-        ToggleButton toggle = (ToggleButton) findViewById(R.id.energyToggleButton); boolean energy = toggle.isChecked();
-        String percentString;
-        if(energy) {
-            percentString = "positive";
-        }
-        else {
-            percentString="negative";
-        }
-        Switch meditate_switch = (Switch) findViewById(R.id.meditate_switch);
-        boolean meditate = meditate_switch.isChecked();
 
-        feeling.setText(nameValue + " is " + percentString + " % Lorelai Gilmore");
+        RatingBar paulAnka = (RatingBar) findViewById(R.id.ratingBar);
+        double ratingValue = paulAnka.getRating();
+        percent = percent + (2.858*ratingValue);
+
+        TextView feeling = (TextView) findViewById(R.id.feelingTextView);
+
+        Spinner breakfastSpinner = (Spinner) findViewById(R.id.spinner);
+        String breakfastValue = String.valueOf(breakfastSpinner.getSelectedItem());
+        if(breakfastValue.equals("Poptart")){
+
+            percent = percent + 14.29;
+        }
+
+        ToggleButton toggle = (ToggleButton)findViewById(R.id.spouseToggleButton);
+        boolean spouse = toggle.isChecked();
+        String spouseString;
+        if(!spouse){
+             percent = percent + 14.29;
+        }
+
+        Switch ex_switch=(Switch)findViewById(R.id.ex_switch);
+        boolean meditate = ex_switch.isChecked();
+        if (meditate){
+            percent = percent + 14.29;
+        }
+        RadioGroup coffee =(RadioGroup)findViewById(R.id.coffeeRadioGroup);
+        int coffee_id=coffee.getCheckedRadioButtonId();
+        switch(coffee_id){
+            case -1:
+                break;
+            case R.id.radioButton:
+                break;
+            case R.id.radioButton2:
+                percent = percent + 43.00;
+                break;
+        }
+        double percentPrinted = Math.round(percent);
+        feeling.setText(nameValue + " is " + percentPrinted + " % Lorelai Gilmore");
     }
 
 }
