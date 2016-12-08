0b3ddb5c3768bd0faa6b30d7cc61598b75aebc7c
Zach Lamb <zala3440@colorado.edu>
2016-12-08 12:09:07 -0700

    Here is what is working: enter text,spinner,change imageview,and button

--- Final/IceCream/app/src/main/java/zach/icecream/MainActivity.java
+++ Final/IceCream/app/src/main/java/zach/icecream/MainActivity.java
@@ -2,6 +2,11 @@ package zach.icecream;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void chooseIceCream(View view){
+        EditText mealType = (EditText) findViewById(R.id.myMeal);
+        String mealValue = mealType.getText().toString();
+
+        Spinner flavorSpinner = (Spinner) findViewById(R.id.spinner);
+        String flavorValue = String.valueOf(flavorSpinner.getSelectedItem());
+
+        TextView output =(TextView) findViewById(R.id.textViewOutput);
+
+        ImageView img= (ImageView) findViewById(R.id.imageView);
+
+        if(flavorValue.equals("Pumpkin Pie")){
+            img.setImageResource(R.drawable.caramel);
+        }
+        else if(flavorValue.equals("Cookie Dough")){
+            img.setImageResource(R.drawable.chocolate);
+        }
+        else{
+            img.setImageResource(R.drawable.cookiescream);
+        }
+
+        output.setText("My favorite\n" + mealValue + " is\n" + flavorValue);
     }
 }
